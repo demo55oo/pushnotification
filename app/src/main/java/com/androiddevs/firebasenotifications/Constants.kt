7835f62c8e1f8aff8c9a302b2fa4ebd51c54056b
ASUS <adhammansour886@gmail.com>
2021-07-03 13:35:08 +0200

    Push_notification

---
+++ app/src/main/java/com/androiddevs/firebasenotifications/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.firebasenotifications
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAK-YrOaw:APA91bGFxpYR6PfoxSR5Govy6E7XV6XtUd1UU75zI0h1EfUcimjFnN7hKITNLY40-ttM-YYlPJqTJUMrNSCgeJpe1mui2mhPtiM3x4DGvbX3cBxtKbLfFDCy8rJFXcORZnZC54P23d4a"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
---
+++ app/src/main/java/com/androiddevs/firebasenotifications/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.androiddevs.firebasenotifications
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+const val TOPIC = "/topics/myTopic2"
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       //FirebaseService.sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+      // FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener {
+//           etToken.setText(it.token)
+        //}
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+
+        btnSend.setOnClickListener {
+            val title = etTitle.text.toString()
+            val message = etMessage.text.toString()
+            //val recipientToken = etToken.text.toString()
+            if(title.isNotEmpty() && message.isNotEmpty() ) {
+                PushNotification(
+                        NotificationData(title, message),
+                    TOPIC
+
+                ).also {
+                    sendNotification(it)
+                }
+            }
+        }
+    }
+
+    private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = RetrofitInstance.api.postNotification(notification)
+            if(response.isSuccessful) {
+                Log.d(TAG, "Response: ${Gson().toJson(response)}")
+            } else {
+                Log.e(TAG, response.errorBody().toString())
+            }
+        } catch(e: Exception) {
+            Log.e(TAG, e.toString())
+        }
+    }
+}
